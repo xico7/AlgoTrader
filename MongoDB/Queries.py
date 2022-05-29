f58e89bb1caf9d6e827ec198b3ede1a5c49d8617
fcs <francisco.conceicaosilva7@gmail.com>
2022-05-29 19:05:56 +0100

    finish 10 secs parsedtrades

--- MongoDB/Queries.py
+++ MongoDB/Queries.py
@@ -5,10 +5,10 @@ from MongoDB.db_actions import connect_to_aggtrade_data_db
 from tasks.transform_trade_data import EVENT_TS
 
 
-def query_aggtrade_data_col_timeframe(symbol, ts_begin, timeframe_in_secs):
+def query_aggtrade_data_col_timeframe(symbol, ts_begin, range_in_ms):
     return list(connect_to_aggtrade_data_db().get_collection(symbol).find(
         {MongoDB.AND: [{EVENT_TS: {MongoDB.HIGHER_EQ: ts_begin}},
-                       {EVENT_TS: {MongoDB.LOWER_EQ: ts_begin + sec_to_ms(timeframe_in_secs)}}]}))
+                       {EVENT_TS: {MongoDB.LOWER_EQ: ts_begin + range_in_ms}}]}))
 
 
 
