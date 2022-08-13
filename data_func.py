dc1a25f90cc926d926d7ef6f6c7df289b3337e42
fcs <francisco.conceicaosilva7@gmail.com>
2022-08-13 15:06:37 +0100

    Improve function call to allow multiple functions.

--- data_func.py
+++ data_func.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from support.decorators_extenders import init_only_existing
 from vars_constants import PRICE, QUANTITY, SYMBOL, DB_TS, DEFAULT_PARSE_INTERVAL, ONE_HOUR_IN_MS, \
     PARSED_TRADES_BASE_DB, PARSED_AGGTRADES_DB
@@ -5,6 +7,9 @@ from dataclasses import dataclass
 from MongoDB.db_actions import insert_bundled_aggtrades, insert_parsed_aggtrades, connect_to_parsed_aggtrade_db, \
     insert_many_db, query_starting_ts, query_existing_ws_trades
 from data_staging import round_last_ten_secs, current_milli_time
+
+
+class InvalidParametersProvided(Exception): pass
 
 
 class CacheAggtrades(dict):
@@ -49,25 +54,32 @@ class Aggtrade:
         return args, kwargs
 
 
-class SymbolsTimeframeTrade:
+class Trade:
 
-    def __init__(self,
-                 timeframe_in_ms=ONE_HOUR_IN_MS,
-                 db_name=PARSED_TRADES_BASE_DB,
+    def __init__(self, symbols=None, timeframe_in_ms=None, parse_interval_in_secs=None, copy_trade=None):
+
+        if not copy_trade and not (symbols and timeframe_in_ms and parse_interval_in_secs):
+            raise InvalidParametersProvided("Invalid parameters provided")
+
+        self.start_ts = copy_trade.start_ts if copy_trade else {}
+        self.end_ts = copy_trade.end_ts if copy_trade else {}
+        self.ts_data = copy_trade.ts_data if copy_trade else {}
+        self.start_price = copy_trade.start_price if copy_trade else {}
+        self.end_price = copy_trade.end_price if copy_trade else {}
+        self._symbols = copy_trade._symbols if copy_trade else symbols
+        self.timeframe = copy_trade.timeframe if copy_trade else timeframe_in_ms
+        self.ms_parse_interval = copy_trade.ms_parse_interval if copy_trade else parse_interval_in_secs * 1000
+
+
+class SymbolsTimeframeTrade(Trade):
+
+    def __init__(self, timeframe_in_ms=ONE_HOUR_IN_MS, db_name=PARSED_TRADES_BASE_DB,
                  parse_interval_in_secs=DEFAULT_PARSE_INTERVAL,
-                 symbols=connect_to_parsed_aggtrade_db().list_collection_names(),
-                 start_ts=None,
-                 end_ts=None):
+                 symbols=connect_to_parsed_aggtrade_db().list_collection_names(), start_ts=None, end_ts=None):
 
-        self.ms_parse_interval = parse_interval_in_secs * 1000
-        self.end_ts = {}
-        self.start_ts = {}
-        self.ts_data = {}
-        self.start_price = {}
-        self.end_price = {}
-        self._symbols = symbols
+        super().__init__(symbols=symbols, timeframe_in_ms=timeframe_in_ms, parse_interval_in_secs=parse_interval_in_secs)
+
         self.db_name = db_name.format(parse_interval_in_secs)
-        self.timeframe = timeframe_in_ms
         self._init_start_end_ts(start_ts=start_ts, end_ts=end_ts)
         self._init_start_end_price()
 
@@ -139,24 +151,24 @@ class SymbolsTimeframeTrade:
         self._init_start_end_ts(self.start_ts, self.end_ts)
 
 
-class FundTimeframeTrade:
+class FundTimeframeTrade(Trade):
     def __init__(self):
         from data_staging import coin_ratio_marketcap
         from vars_constants import SP500_SYMBOLS_USDT_PAIRS
         from MongoDB.db_actions import query_parsed_aggtrade_multiple_timeframes
-        ratios, fund_marketcap = coin_ratio_marketcap()
+
         start_ts = 0
         for symbol in SP500_SYMBOLS_USDT_PAIRS:
             ts = query_starting_ts(PARSED_TRADES_BASE_DB.format(DEFAULT_PARSE_INTERVAL), symbol)
             if ts > start_ts:
                 start_ts = ts
 
-        parse_tf_trades = SymbolsTimeframeTrade(start_ts=start_ts)
+        parse_tf_trades = SymbolsTimeframeTrade(symbols=SP500_SYMBOLS_USDT_PAIRS, start_ts=start_ts)
         parse_tf_trades += query_parsed_aggtrade_multiple_timeframes(
             SP500_SYMBOLS_USDT_PAIRS, parse_tf_trades.start_ts, parse_tf_trades.end_ts)
 
-        print("here")
-            #separar o init data ts e price.. aqui criar um objeto com o startts já definido
-            #start_ts
-            #     range(symbols_tf_trades.start_ts['BTCUSDT'], symbols_tf_trades.end_ts['BTCUSDT'], symbols_tf_trades.ms_parse_interval)
-            # pass #if btc_timeframe
+        super().__init__(copy_trade=parse_tf_trades)
+
+        ratios, fund_marketcap = coin_ratio_marketcap()
+        self.ratios = ratios
+        self.fund_marketcap = fund_marketcap
